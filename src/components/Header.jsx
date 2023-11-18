17dcfef1b052dd38a710bbf6d40e99081bb7dfa1
danilRomin <dvromin@yandex.ru>
2023-11-18 19:23:27 +0700

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className="header">
+                <img
+                    src="https://avatars.yandex.net/get-music-user-playlist/71140/595193671.1017.52340/m1000x1000?1664977781745&webp=false"
+                    alt="Фотография"/>
+            </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://media.istockphoto.com/id/1288385045/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D1%81%D0%BD%D0%B5%D0%B6%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9-%D0%BF%D0%B8%D0%BA-k2.jpg?s=612x612&w=0&k=20&c=MxlQOac2Ud_KNb1lWhpSTyJjHUgVqTK2VZMOQDtozCw="
+                    alt="Обложка"/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
